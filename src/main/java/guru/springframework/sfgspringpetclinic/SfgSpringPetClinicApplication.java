89188a94fa136e7de292aeda893c1bf9060c5be6
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-27 21:42:29 +0800

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfgspringpetclinic/SfgSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgSpringPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgspringpetclinic/SfgSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
