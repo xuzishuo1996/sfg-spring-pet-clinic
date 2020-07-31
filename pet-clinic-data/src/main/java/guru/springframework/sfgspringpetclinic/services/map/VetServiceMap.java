1726435317f3a0f6becba97ec0c27e5bb53fb567
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-31 11:06:26 +0800

    add bootstrap for data. Closes #14

--- pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package guru.springframework.sfgspringpetclinic.services.map;
 
 import guru.springframework.sfgspringpetclinic.model.Vet;
-import guru.springframework.sfgspringpetclinic.services.CrudService;
+import guru.springframework.sfgspringpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
