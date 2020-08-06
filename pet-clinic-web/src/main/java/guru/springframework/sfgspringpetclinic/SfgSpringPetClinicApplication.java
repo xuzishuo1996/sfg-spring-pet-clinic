31ab31b1b7575a3ad1746e6ec0227fa3e1cab5a6
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-08-06 16:12:18 +0800

    add @ExtendWith and revise pom.xml. close #4

--- pet-clinic-web/src/main/java/guru/springframework/sfgspringpetclinic/SfgSpringPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgspringpetclinic/SfgSpringPetClinicApplication.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgspringpetclinic;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootApplication
 public class SfgSpringPetClinicApplication {
 
