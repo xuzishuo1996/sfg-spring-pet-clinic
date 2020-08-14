2f4792d5a2f4e246fdb622449536c16499bdcc59
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-08-14 15:16:29 +0800

    impl find owner. closes #54

--- pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package guru.springframework.sfgspringpetclinic.services;
 
 import guru.springframework.sfgspringpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String LastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgspringpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgspringpetclinic/controllers/OwnerController.java
@@ -1,12 +1,18 @@
 package guru.springframework.sfgspringpetclinic.controllers;
 
+import guru.springframework.sfgspringpetclinic.model.Owner;
 import guru.springframework.sfgspringpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RequestMapping("/owners")
 @Controller
@@ -18,18 +24,37 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/index", "/index.html"})
-    public String listOwners(Model model) {
-
-        model.addAttribute("owners", ownerService.findAll());
-
-        return "owners/index";
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping("/find")
-    public String findOwners() {
+    public String findOwners(Model model) {
+        model.addAttribute("owner", Owner.builder().build());
+        return "owners/findOwners";
+    }
 
-        return "notimplemented";
+    @GetMapping
+    public String processFindForm(Owner owner, BindingResult result, Model model) {
+        //allow parameterless GET request for /owners to return all records
+        if (owner.getLastName() == null) {
+            owner.setLastName("");
+        }
+
+        //find owners by last name
+        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+
+        if (results.isEmpty()) {
+            result.rejectValue("lastName", "notFound", "not found");
+            return "owners/findOwners";
+        } else if (results.size() == 1) {
+            owner = results.get(0);
+            return "redirect:/owners/" + owner.getId();
+        } else {
+            model.addAttribute("selections", results);
+            return "owners/ownersList";
+        }
     }
 
     @GetMapping("/{ownerId}")
