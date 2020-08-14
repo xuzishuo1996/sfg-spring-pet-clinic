d54fba7b45f4a221e0da57d4866632cc5f18e291
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-08-14 18:23:59 +0800

    fix the bug of displaying newly-added Pet. closes #71

--- pet-clinic-web/src/main/java/guru/springframework/sfgspringpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgspringpetclinic/controllers/PetController.java
@@ -53,6 +53,7 @@ public class PetController {
         Pet pet = new Pet();
         owner.getPets().add(pet);
         pet.setOwner(owner);
+//        petService.save(pet);
         model.addAttribute("pet", pet);
         return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
     }
@@ -67,6 +68,7 @@ public class PetController {
             model.put("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
+            pet.setOwner(owner);
             petService.save(pet);
 
             return "redirect:/owners/" + owner.getId();
